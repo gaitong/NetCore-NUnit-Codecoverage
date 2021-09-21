b99f003c70bbe8afdc2fe45378acbc8bdee28bf0
gaitong <gaitong.p@gmail.com>
2021-09-21 21:25:40 +0700

    init my app

---
+++ NUnitTest/CalculateScoreServiceTest.cs
@@ -0,0 +1,90 @@
+﻿using MyNUnitDemo.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTest
+{
+    public class CalculateScoreServiceTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        //given-when-then
+        [Test]
+        public void Score73_Execute_True()
+        {
+            var calc = new CalculateScoreService();
+            var res = calc.Execute("73");
+
+            //check unit
+            Assert.True(res);
+        }
+
+        [Test]
+        public void Score50_Execute_True()
+        {
+            var calc = new CalculateScoreService();
+            var res = calc.Execute("50");
+
+            //check unit
+            Assert.True(res);
+        }
+
+        [Test]
+        public void Score21_Execute_False()
+        {
+            var calc = new CalculateScoreService();
+            var res = calc.Execute("21");
+
+            //check unit
+            Assert.False(res);
+        }
+
+        [Test]
+        public void ScoreXXX_Execute_ThrowException()
+        {
+            var expected = "Error! Please input number.";
+            var calc = new CalculateScoreService();
+            //var res = calc.Execute("xxx");
+            var res = Assert.Throws<Exception>(() => calc.Execute("xxx"));
+
+            //check unit
+            Assert.AreEqual(expected, res.Message);
+        }
+
+        [Test]
+        public void ScoreNull_Execute_ThrowException()
+        {
+            var expected = "Error! Please input number.";
+            var calc = new CalculateScoreService();
+            //var res = calc.Execute("xxx");
+            var res = Assert.Throws<Exception>(() => calc.Execute(null));
+
+            //check unit
+            Assert.AreEqual(expected, res.Message);
+        }
+
+        [Test]
+        public void Score500Point25_Execute_ThrowException()
+        {
+            var expected = "Error! Please input number.";
+            var calc = new CalculateScoreService();
+            //var res = calc.Execute("xxx");
+            var res = Assert.Throws<Exception>(() => calc.Execute("500.25"));
+
+            //check unit
+            Assert.AreEqual(expected, res.Message);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
